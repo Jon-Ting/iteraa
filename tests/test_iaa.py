1baeff0cb48f62d878a38ec28f9c282749c5c8b5
Ting, Jonathan <jonathant@diversityarrays.com>
2025-01-27 00:11:37 +1100

    test: fix mistakes in test_iaa.py

--- tests/test_iaa.py
+++ tests/test_iaa.py
@@ -3,7 +3,7 @@ from os import listdir
 from iaa.constants import RANDOM_STATE, NUM_JOBS, PALETTE, DPI, SUBSETS_PICKLES_PATH, OUTPUTS_PICKLES_PATH, FIGS_DIR_PATH
 # from iaa.datasets import getExampleDataPath
 from iaa.utils import explainedVariance
-from iaa.plot import ternaryPlot, compareProfile, datapointProfile, plotRadarDatapoint, createSimplexAx, mapAlfaToSimplex, plotTSNE
+from iaa.plot import plotRadarDatapoints, createSimplexAx, mapAlfaToSimplex, plotTSNE
 from iaa.iaa import ArchetypalAnalysis
 from iaa.piaa import subsetSplit, runAA, fitPIAA
 
