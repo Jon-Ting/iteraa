667def84032ca79ab8f6d07c963cfee21a9f175a
Ting, Jonathan <jonathant@diversityarrays.com>
2025-01-27 23:44:55 +1100

    build: added python-semantic-release

--- tests/test_iteraa.py
+++ tests/test_iteraa.py
@@ -1,11 +1,11 @@
 from os import listdir
 
-from iteraa.constants import RANDOM_STATE, NUM_JOBS, PALETTE, DPI, SUBSETS_PICKLES_PATH, OUTPUTS_PICKLES_PATH, FIGS_DIR_PATH
+#from iteraa.constants import RANDOM_STATE, NUM_JOBS, PALETTE, DPI, SUBSETS_PICKLES_PATH, OUTPUTS_PICKLES_PATH, FIGS_DIR_PATH
 # from iteraa.datasets import getExampleBlobDataPath
-from iteraa.utils import explainedVariance
-from iteraa.plot import plotRadarDatapoints, createSimplexAx, mapAlfaToSimplex, plotTSNE
-from iteraa.iaa import ArchetypalAnalysis
-from iteraa.piaa import subsetSplit, runAA, fitPIAA
+#from iteraa.utils import explainedVariance
+#from iteraa.plot import plotRadarDatapoints, createSimplexAx, mapAlfaToSimplex, plotTSNE
+#from iteraa.iaa import ArchetypalAnalysis
+#from iteraa.piaa import subsetSplit, runAA, fitPIAA
 
 
 #def test_subsetSplit(X, nSubsets, dataName):
