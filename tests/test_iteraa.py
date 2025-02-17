ba149f102af2a9913705c7860c6b094e5f790851
Ting, Jonathan <jonathant@diversityarrays.com>
2025-02-18 02:24:43 +1100

    test: initialised tests for core functions

--- tests/test_iteraa.py
+++ tests/test_iteraa.py
@@ -1,4 +1,8 @@
+from natsort import natsorted
+import numpy as np
 from os import listdir
+from os.path import isfile
+from pytest import approx
 
 from iteraa.constants import RANDOM_STATE, NUM_JOBS, PALETTE, DPI, SUBSETS_PICKLES_PATH, OUTPUTS_PICKLES_PATH, FIGS_DIR_PATH
 from iteraa.datasets import getExampleBlobData, getExampleSquareData, getCaseStudyData
@@ -8,32 +12,71 @@ from iteraa.iaa import ArchetypalAnalysis
 from iteraa.piaa import subsetSplit, runAA, fitPIAA
 
 
+NUM_SAMPLES = 100
+NUM_ARCHETYPES = 3
+NUM_SUBSETS = 3
+
+
 def test_getExampleBlobData():
-    numSamples = 100
-    X = getExampleBlobData(numSamples)
-    assert X.shape == (numSamples, 2)
+    """Unit test of getExampleBlobData()."""
+    X = getExampleBlobData(NUM_SAMPLES)
+    assert X.shape == (NUM_SAMPLES, 2)
 
 
 def test_getExampleSquareData():
+    """Unit test of getExampleSquareData()."""
     numSamples = 100
-    X = getExampleSquareData(numSamples)
-    assert X.shape == (numSamples, 2)
+    X = getExampleSquareData(NUM_SAMPLES)
+    assert X.shape == (NUM_SAMPLES, 2)
 
 
 def test_getCaseStudyData():
+    """Unit test of getCaseStudyData()."""
     X = getCaseStudyData()
-    print(X)
-    assert X.columns == ['AnkleDiam', 'KneeDiam', 'WristDiam', 'Bitro', 'Biil', 'ElbowDiam', 'ChestDiam', 'ChestDp', 'Biac', 'Height']
-    assert X.shape == (numSamples, 2)
+    assert X.shape == (507, 10)
 
 
-# def test_subsetSplit(X, nSubsets, dataName):
-#     """Unit test of subsetSplit()."""
-#     subsetSplit(X, nSubsets, dataName)
-#     assert len(listdir(f"{}")) == 0
+def test_explainedVariance():
+    """Unit test of explainedVariance()."""
+    Xact = getExampleBlobData(NUM_SAMPLES)
+    assert explainedVariance(Xact, Xact) == approx(1.0)
+
+
+def test_ArchetypalAnalysis():
+    """Unit test of ArchetypalAnalysis()."""
+    aa = ArchetypalAnalysis(nArchetypes=NUM_ARCHETYPES, iterative=True)
+    X = getExampleSquareData(NUM_SAMPLES)
+    aa.fit(X)
+    assert aa.nDim == 2
+    assert aa.nData == NUM_SAMPLES
+    assert aa.alfa.shape == (NUM_ARCHETYPES, NUM_SAMPLES)
+    assert aa.beta.shape == (NUM_SAMPLES, NUM_ARCHETYPES)
+    assert aa.archetypes.shape == (2, NUM_ARCHETYPES)
+
+
+def test_subsetSplit():
+    """Unit test of subsetSplit()."""
+    subsetsPicklesPath, dataName = 'tests/subsetsDataPKLs', 'test'
+    X = getExampleSquareData(NUM_SAMPLES)
+    subsetSplit(X, nSubsets=NUM_SUBSETS, dataName=dataName, subsetsPicklesPath=subsetsPicklesPath)
+    dirFiles = listdir(subsetsPicklesPath)
+    assert len(dirFiles) == NUM_SUBSETS
+    for (i, dirFile) in enumerate(natsorted(dirFiles)):
+        assert dirFile.startswith(f"{dataName}data{i+1}.pkl")
 
 
 def test_runAA():
     """Unit test of runAA()."""
-    assert 1 == 1
+    dataName, subsetID = 'test', 1
+    fName = f"tests/subsetsDataPKLs/{dataName}data{subsetID}.pkl"
+    outputsPicklesPath = 'tests/subsetsOutputsPKLs'
+    runAA(fName, nArchetypes=NUM_ARCHETYPES, outputsPicklesPath=outputsPicklesPath)
+    assert isfile(f"{outputsPicklesPath}/{dataName}output{subsetID}.pkl")
+
+
+def test_fitPIAA():
+    """Unit test of fitPIAA()."""
+    pass
+    # fitPIAA()
+
 
